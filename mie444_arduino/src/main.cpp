b8f4d8be3afb1ff4f90d2a17b3fc7312976fe9cc
hschier <hudsonschier@gmail.com>
2019-11-02 19:19:57 -0400

    motortest

--- mie444_arduino/src/main.cpp
+++ mie444_arduino/src/main.cpp
@@ -12,8 +12,6 @@
 
 //initializing all the variables
 const int LOOPTIME                      100     //Looptime in millisecond
-const byte noCommLoopMax = 10;                //number of main loops the robot will execute without communication before stopping
-unsigned int noCommLoops = 0;                 //main loop without communication counter
 
 const int PIN_ENCOD_A_MOTOR_LEFT = 18;               //A channel for encoder of left motor                    
 const int PIN_ENCOD_B_MOTOR_LEFT = 19;               //B channel for encoder of left motor
@@ -23,25 +21,14 @@ const int PIN_ENCOD_B_MOTOR_RIGHT = 21;              //B channel for encoder of 
 const int ENCODER_TICKS_PER_REV = 32;
 const int ENCODER_TICKS_TOLERANCE = 1;               //32 ticks if counting rising and falling for one encoder channel
 
-unsigned long lastMilli = 0;
 const double radius = 0.034;                   //Wheel radius, in m
 const double wheelbase = 0.2;               //Wheelbase, in m
 
-double speed_req = 0;                         //Desired linear speed for the robot, in m/s
-double angular_speed_req = 0;                 //Desired angular speed for the robot, in rad/s
+// Define motors
 
-double speed_req_left = 0;                    //Desired speed for left wheel in m/s
-double speed_act_left = 0;                    //Actual speed for left wheel in m/s
-double speed_cmd_left = 0;                    //Command speed for left wheel in m/s 
-
-double speed_req_right = 0;                   //Desired speed for right wheel in m/s
-double speed_act_right = 0;                   //Actual speed for right wheel in m/s
-double speed_cmd_right = 0;                   //Command speed for right wheel in m/s 
-
-const double max_speed = 255;                 //Max speed in m/s
-
-int PWM_leftMotor = 0;                     //PWM command for left motor
-int PWM_rightMotor = 0;                    //PWM command for right motor 
+// EN, IN1, IN2
+L298N L_motor(5, 6, 7);
+L298N R_motor(8, 9, 10);
 
 // PID Parameters
 const double PID_left_param[] = { 1, 0.1, 0.1 }; //Respectively Kp, Ki and Kd for left motor PID
@@ -54,10 +41,31 @@ PID PID_leftMotor(&speed_act_left, &speed_cmd_left, &speed_req_left, PID_left_pa
 PID PID_rightMotor(&speed_act_right, &speed_cmd_right, &speed_req_right, PID_right_param[0], PID_right_param[1], PID_right_param[2], DIRECT);   //Setting up the PID for right motor
 
 void setup() {
-
+	Serial.begin(115200);
+	L_motor.stop();
+	R_motor.stop();
 }
 
-
+void loop() {
+	L_motor.setSpeed(255);
+	R_motor.setSpeed(255);
+	L_motor.forward();
+	R_motor.forward();
+	delay(3000);
+	L_motor.stop();
+	R_motor.stop();
+	delay(1000);
+	L_motor.setSpeed(255);
+	R_motor.setSpeed(255);
+	L_motor.backward();
+	R_motor.forward();
+	delay(1000);
+	L_motor.setSpeed(255);
+	R_motor.setSpeed(255);
+	L_motor.forward();
+	R_motor.backward();
+	delay(1000);
+}
 
 //Left motor encoder counter
 void encoderLeftMotor() {
